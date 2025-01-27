b6054150f92a9f275e2722bff82be3e197c3493d
Filip <fipe2200@student.miun.se>
2025-01-27 13:00:33 +0100

    Lagt in knapp

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.test2;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     ImageView imageView;
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,20 +25,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-        ImageView image = findViewById(R.id.weatherImage);
+        imageView = findViewById(R.id.weatherImage);
+        button = findViewById(R.id.refresh);
 
         // Fetch weather details
         WeatherDataManager.getWeatherSummary(new WeatherDataManager.WeatherSummaryCallback() {
             @Override
             public void onSuccess(WeatherSummary weatherSummary) {
                 String symbolcode = weatherSummary.getWeatherSymbol();
-                image.setImageResource(getSymbolImage(symbolcode));
+                imageView.setImageResource(getSymbolImage(symbolcode));
                 // Display the weather information
                 textView.setText(
                             "Air Temperature: " + weatherSummary.getTemperature() + " °C\n" +
                                     "Wind Speed: " + weatherSummary.getWindSpeed() + " m/s\n" +
                                     "Wind Direction: " + weatherSummary.getWindDirection() + " \n"
                     );
+                button.setVisibility(View.VISIBLE);
+
+                button.setOnClickListener(click ->{
+
+                });
             }
 
 
